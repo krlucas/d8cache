1913867c71e5a49146b4d5eb5f3942e8e943ac60
Kelly Lucas <krlucas@gmail.com>
2018-11-16 12:21:36 -0500

    Initialize d8 cache example module.

---
+++ src/Plugin/Block/D8CachePoweredByBlock.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Drupal\d8cache\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Session\AccountProxyInterface;
+
+/**
+ * Provides a 'D8CachePoweredByBlock' block.
+ *
+ * @Block(
+ *  id = "d8cachepowered_by_block",
+ *  admin_label = @Translation("Powered by Drupal and YOU"),
+ * )
+ */
+class D8CachePoweredByBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * Drupal\Core\Session\AccountProxyInterface definition.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $currentUser;
+  /**
+   * Constructs a new Ned2018PoweredByBlock object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param string $plugin_definition
+   *   The plugin implementation definition.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    AccountProxyInterface $current_user
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->currentUser = $current_user;
+  }
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('current_user')
+    );
+  }
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [
+      '#markup' => '<span>' . $this->t('Powered by <a href=":poweredby">Drupal</a>' . ' and @current_user', [
+          ':poweredby' => 'https://www.drupal.org',
+          '@current_user' => $this->currentUser->getDisplayName(),
+        ]) . '</span>'
+    ];
+    return $build;
+  }
+
+}
